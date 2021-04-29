73ff02e84fc5d861924f5515cc629ed71ac2e632
Ranjit Jana <40226912+ranjitjana027@users.noreply.github.com>
2021-04-29 14:41:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ app.config["SESSION_PERMANENT"]=False
 app.config["SESSION_TYPE"]='filesystem'
 Session(app)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-socketio = SocketIO(app, cors_allowed_origins=['https://flackweb.netlify.app','https://ranjitjana027.github.io/flackweb-react/'])
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 # Configuartion for SQLAlchemy
 
